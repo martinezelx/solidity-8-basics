7b44276d4c5a59bb8d46f53147334c7a784306ef
Luis Martínez <36764797+martinezelx@users.noreply.github.com>
2022-05-26 13:43:02 +0200

    Create 8-compare_strings.sol

---
+++ 8-compare_strings.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
+
+contract compareStrings {
+    
+    function compare(string memory _j ,string memory _i) public pure returns(bool) {
+        
+        bytes32 hash_i = keccak256(abi.encodePacked(_i));
+        bytes32 hash_j = keccak256(abi.encodePacked(_j));
+        
+        if(hash_i == hash_j){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
