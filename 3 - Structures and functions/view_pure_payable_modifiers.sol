c3c3d25a6c2fa29b6fdddc901a55d90df7b1cace
Luis Martínez <36764797+martinezelx@users.noreply.github.com>
2022-06-20 17:50:25 +0200

    Create view_pure_payable_modifiers.sol

---
+++ 3 - Structures and functions/view_pure_payable_modifiers.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract view_pure_payable {
+    
+    //Modificador de view
+    string[] lista_alumnos;
+    
+    function nuevo_alumno(string memory _alumno) public {
+        lista_alumnos.push(_alumno);
+    }
+    
+    function ver_alumno(uint _posicion) public view returns(string memory) {
+        return lista_alumnos[_posicion];
+    }
+    
+    uint x=10;
+    function sumarAx(uint _a) public view returns(uint) {
+        return x+_a;
+    }
+    
+    //Modificador de pure
+    
+    function exponenciacion(uint _a, uint _b) public pure returns(uint) {
+        return _a**_b;
+    }
+    
+    //Modificador de payable
+    
+    mapping(address=>cartera) DineroCartera;
+    
+    struct cartera {
+        string nombre_persona;
+        address direccion_persona;
+        uint dinero_persona;
+    }
+    
+    function Pagar(string memory _nombrePersona, uint _cantidad) public payable {
+        cartera memory mi_cartera;
+        mi_cartera = cartera(_nombrePersona, msg.sender, _cantidad);
+        DineroCartera[msg.sender] = mi_cartera;
+    }
+    
+    function verSaldo() public view returns(cartera memory _cartera) {
+        return DineroCartera[msg.sender];
+    }
+}
