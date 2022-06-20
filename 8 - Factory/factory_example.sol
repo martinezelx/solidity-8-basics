ebf876ca7ff3a30b60d2fdaa5d4c742e6868f02d
Luis Martínez <36764797+martinezelx@users.noreply.github.com>
2022-06-20 18:08:29 +0200

    Create factory_example.sol

---
+++ 8 - Factory/factory_example.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SmartContract1 {
+    
+    // Almacenamiento de la informacion del Factory
+    mapping (address => address) public MiContratoPersonal;
+    
+    function Factory() public {
+        address direccion_nuevo_contrato = address (new SmartContract2(msg.sender)); 
+        MiContratoPersonal[msg.sender] = direccion_nuevo_contrato;
+    }
+}
+
+contract SmartContract2 {
+    
+    address public owner;
+
+    constructor (address _direccion) {
+        owner = _direccion;
+    }  
+}
+
